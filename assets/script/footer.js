fd6c35962b94a0ba53212cd13ba82785ac4ce5e8
trannkiennquangg-netizen <trannkiennquangg@gmail.com>
2026-03-01 14:59:39 +0700

    update

--- assets/script/footer.js
+++ assets/script/footer.js
@@ -1,4 +1,4 @@
-fetch('./footer.html')
+fetch('../footer.html')
 .then(response => response.text())
 .then(data => {
     document.getElementById('footer1').innerHTML = data;
--- assets/script/header.js
+++ assets/script/header.js
@@ -1,4 +1,4 @@
-fetch('./assets/header.html')
+fetch('../header.html')
 .then(response => response.text())
 .then(data => {
     document.getElementById('header1').innerHTML = data;
